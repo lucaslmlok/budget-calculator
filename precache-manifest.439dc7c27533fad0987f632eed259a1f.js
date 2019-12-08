a8191160dfbafdd299eaa6411075c8ccde575983
lucas.lmlok <lucas.lmlok@gmail.com>
2019-12-08 22:29:16 +0800

    Updates

---
+++ precache-manifest.439dc7c27533fad0987f632eed259a1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c493a6703231673d7a0c610c7969b81b",
+    "url": "/budget-calculator/index.html"
+  },
+  {
+    "revision": "db88cc7d76efb4a7a65c",
+    "url": "/budget-calculator/static/css/main.d12a1485.chunk.css"
+  },
+  {
+    "revision": "267a4f6dfa352ffc3499",
+    "url": "/budget-calculator/static/js/2.e4d0423d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/budget-calculator/static/js/2.e4d0423d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db88cc7d76efb4a7a65c",
+    "url": "/budget-calculator/static/js/main.36b66be2.chunk.js"
+  },
+  {
+    "revision": "c11c997fff2ac5fa5aaf",
+    "url": "/budget-calculator/static/js/runtime-main.78f34a88.js"
+  }
+]);
